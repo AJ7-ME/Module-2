118fe86b185107b0b7805effc22ea0b0577df212
AJ7-ME <aaravkjoshi2013@gmail.com>
2026-01-14 18:22:22 +1100

    yees

---
+++ Homework/Turtle.py
@@ -0,0 +1,12 @@
+import turtle
+x = turtle.Turtle()
+x.forward(100)
+x.left(90)
+x.forward(100)
+x.left(90)
+x.forward(100)
+x.left(90)
+x.forward(100)
+while True:
+    x.left(99999999999999)
+#it is partying because it finished the square
